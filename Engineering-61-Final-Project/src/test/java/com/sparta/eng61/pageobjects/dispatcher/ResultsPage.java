b5502eef3d6561b899aa36c01564c8decfafd7fc
MahamudHub <mo.mahamud@outlook.com>
2020-08-05 12:16:39 +0100

    Results page class update commit

--- Engineering-61-Final-Project/src/test/java/com/sparta/eng61/pageobjects/dispatcher/ResultsPage.java
+++ Engineering-61-Final-Project/src/test/java/com/sparta/eng61/pageobjects/dispatcher/ResultsPage.java
@@ -11,6 +11,9 @@ public class ResultsPage {
     By candidateName = new By.ByClassName("col-2");
     By candidateEmail = new By.ByClassName("col-3");
     By candidateScore = new By.ByClassName("col-1 font-weight-bold dark");
+    By candidateNR = new By.ByCssSelector("css=tbody .col-1:nth-child(6)");
+    By candidateLR = new By.ByCssSelector("css=tbody .col-1:nth-child(7)");
+    By candidateVandCR = new By.ByCssSelector("css=tbody .col-2:nth-child(8)");
 
     public ResultsPage(WebDriver webDriver) {
         this.webDriver = webDriver;
@@ -32,6 +35,18 @@ public class ResultsPage {
 
     public String getCandidateScore() {
         return webDriver.findElement(candidateScore).getText();
+    }
+
+    public String getCandidateNR() {
+        return webDriver.findElement(candidateNR).getText();
+    }
+
+    public String getCandidateLR() {
+        return webDriver.findElement(candidateLR).getText();
+    }
+
+    public String getCandidateVandCR() {
+        return webDriver.findElement(candidateVandCR).getText();
     }
 
     public String getUpdateButtonTest() {
