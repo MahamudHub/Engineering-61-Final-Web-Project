6e13ae70038321801f22a6c93c7fb976a6f4a807
MahamudHub <mo.mahamud@outlook.com>
2020-08-05 15:33:55 +0100

    PollsPage commit

---
+++ Engineering-61-Final-Project/src/test/java/com/sparta/eng61/pageobjects/dispatcher/PollsPage.java
@@ -0,0 +1,39 @@
+package com.sparta.eng61.pageobjects.dispatcher;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class PollsPage {
+    WebDriver webDriver;
+    By pageTitle = new By.ById("login_title");
+    By pollTime = new By.ByCssSelector("css=tbody > .d-flex:nth-child(1) > .col-3:nth-child(1)");
+    By assessmentStatus = new By.ByCssSelector("css=tbody > .d-flex:nth-child(1) > .col-3:nth-child(2)");
+    By candidateEmail = new By.ByCssSelector("css=tbody > .d-flex:nth-child(1) > .col-3:nth-child(3)");
+    By testId = new By.ByCssSelector("css=tbody > .d-flex:nth-child(1) > .col-3:nth-child(4)");
+
+    public PollsPage(WebDriver webDriver) {
+        this.webDriver = webDriver;
+        webDriver.manage().window().maximize();
+        webDriver.get("https://eng61.spartaglobal.academy/polls");
+    }
+
+    public String getPageTitleName() {
+        return webDriver.findElement(pageTitle).getText();
+    }
+
+    public String getPollTime() {
+        return webDriver.findElement(pollTime).getText();
+    }
+
+    public String getAssessmentStatus() {
+        return webDriver.findElement(assessmentStatus).getText();
+    }
+
+    public String getCandidateEmail() {
+        return webDriver.findElement(candidateEmail).getText();
+    }
+
+    public String getTestId() {
+        return webDriver.findElement(testId).getText();
+    }
+}
