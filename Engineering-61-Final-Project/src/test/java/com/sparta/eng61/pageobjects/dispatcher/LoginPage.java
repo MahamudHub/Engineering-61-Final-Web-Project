96dae1c1a8399151e60dd7890dd9e5256bbc0a8f
S-Mahamed <sumaya_abdalle@outlook.com>
2020-08-05 11:50:45 +0100

    Login and logout commit

--- Engineering-61-Final-Project/src/test/java/com/sparta/eng61/pageobjects/dispatcher/LoginPage.java
+++ Engineering-61-Final-Project/src/test/java/com/sparta/eng61/pageobjects/dispatcher/LoginPage.java
@@ -2,19 +2,19 @@ package com.sparta.eng61.pageobjects.dispatcher;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 
 public class LoginPage {
-    public Properties properties = new Properties();
     WebDriver webDriver;
+
+    private Properties properties = new Properties();
     By username  =  new By.ByName("username");
     By password = new By.ByName("password");
     By submit = new By.ByName("submit");
-    By error = new By.ById("error");
-    By logOutLink = new By.ById("logout_link");
 
 public void propertiesAccess() {
     try {
@@ -24,7 +24,6 @@ public void propertiesAccess() {
         e.printStackTrace();
     }
 }
-
     public LoginPage(WebDriver webDriver) {
         this.webDriver = webDriver;
     }
@@ -33,21 +32,26 @@ public void propertiesAccess() {
         webDriver.get("https://eng61.spartaglobal.academy/login");
         return this;
     }
+
     //right credentials
     public void enterUsername () {
-        System.out.println(properties.getProperty("username").getClass());
-        webDriver.findElement(username).sendKeys(properties.getProperty("username"));
+          webDriver.findElement(username).sendKeys("admin");
+//        System.out.println(properties.getProperty("username").getClass());
+//        webDriver.findElement(username).sendKeys(properties.getProperty("username"));
     }
     public void enterPassword () {
-        webDriver.findElement(password).sendKeys(properties.getProperty("password"));
+        webDriver.findElement(password).sendKeys("!Eng61");
+//        webDriver.findElement(password).sendKeys(properties.getProperty("password"));
     }
 
     //wrong credentials
     public void enterWrongUsername () {
-        webDriver.findElement(username).sendKeys(properties.getProperty("incorrectUsername"));
+        webDriver.findElement(username).sendKeys("admi");
+//        webDriver.findElement(username).sendKeys(properties.getProperty("incorrectUsername"));
     }
     public void enterWrongPassword () {
-        webDriver.findElement(password).sendKeys(properties.getProperty("incorrectPassword"));
+        webDriver.findElement(password).sendKeys("!Eng6");
+        //webDriver.findElement(password).sendKeys(properties.getProperty("incorrectPassword"));
     }
 
     //submit button
@@ -60,51 +64,63 @@ public void propertiesAccess() {
         boolean errorExists = webDriver.getPageSource().contains("Incorrect password, please try to login again!");
         return errorExists;
     }
-    //login directs to new page
-//    public SendAssessmentPage login(){
-//        enterUsername();
-//        enterPassword();
-//        clickSubmitButton();
-//        return new SendAssessmentPage(webDriver);
-//    }
 
-    //submitting right username and right password
-    public void submitRightCredentials () {
-        enterUsername();
-        enterPassword();
-        clickSubmitButton();
-    }
-    //submitting wrong username and right password
+    //======Testing methods========
 
-    public void submitWrongUsername () {
-        enterWrongUsername();
-        enterPassword();
-        clickSubmitButton();
-    }
-    //submitting right username and wrong password
+    //-------------Blank username or password---------------
 
-    public void submitWrongPassword () {
-        enterUsername();
-        enterWrongPassword();
-        clickSubmitButton();
-    }
-    //submitting no username and no password
-    public void submitNoInfo () {
-        clickSubmitButton();
-    }
     //submitting username but no password
     public void submitonlyUsername () {
+        openLoginPage();
         enterUsername();
         clickSubmitButton();
     }
     //submitting no username but right password
     public void submitonlyPassword () {
+        openLoginPage();
+        enterPassword();
+        clickSubmitButton();
+    }
+    //submitting no username and no password
+    public void submitNoInfo () {
+        openLoginPage();
+        clickSubmitButton();
+    }
+
+    //------------------Correct username and password-------------
+    public void submitRightCredentials () {
+        openLoginPage();
+        enterUsername();
         enterPassword();
         clickSubmitButton();
     }
 
-    //Logout from dispaches
-    //Logout from Results
-    //Logout from polls
+    //---------------submitting wrong username and right password------------
+    public void submitWrongUsername () {
+        openLoginPage();
+        enterWrongUsername();
+        enterPassword();
+        clickSubmitButton();
+    }
+
+    //-----------------submitting right username and wrong password-----------
+    public void submitWrongPassword () {
+        openLoginPage();
+        enterUsername();
+        enterWrongPassword();
+        clickSubmitButton();
+    }
+
+
+    //------------------login directs to new page-------------------
+    public SendAssessmentPage login(){
+        openLoginPage();
+        enterUsername();
+        enterPassword();
+        clickSubmitButton();
+        return new SendAssessmentPage(webDriver);
+    }
+
+
 
 }
--- Engineering-61-Final-Project/src/test/java/com/sparta/eng61/pageobjects/dispatcher/LoginTest.java
+++ Engineering-61-Final-Project/src/test/java/com/sparta/eng61/pageobjects/dispatcher/LoginTest.java
@@ -8,45 +8,80 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class LoginTest {
     WebDriver webDriver = new ChromeDriver();
     LoginPage loginPage = new LoginPage(webDriver);
+    DispatchesPage dispatchesPage = new DispatchesPage(webDriver);
+    ResultsPage resultsPage = new ResultsPage(webDriver);
+    PollsPage pollsPage = new PollsPage(webDriver);
+    LogoutPage logoutPage = new LogoutPage(webDriver);
 
-    //        @Test
-//        public void openLogpage(){
-//            loginPage.openLoginPage();
-//            loginPage.login();
-//        }
+    //=======TESTING LOGGING OUT============
+    //------Logging out from the Dispatches page----------
+    @Test
+    public void LoggingoutfromDispatches(){
+        loginPage.submitRightCredentials();
+        dispatchesPage.LogoutfromDispatches();
+        Assertions.assertEquals(true,logoutPage.successfulLogout());
+
+    }
+    //------Logging out from the Results page----------
+    @Test
+    public void LoggingoutfromResults(){
+        loginPage.submitRightCredentials();
+        resultsPage.LogoutfromResults();
+        Assertions.assertEquals(true,logoutPage.successfulLogout());
+
+    }
+    //------Logging out from the Polls page----------
+
+    @Test
+    public void LoggingoutfromPolls(){
+        loginPage.submitRightCredentials();
+        pollsPage.LogoutfromPolls();
+        Assertions.assertEquals(true,logoutPage.successfulLogout());
+
+    }
+    //---------Testing if logout page opens--------
+    @Test
+    public void openLogoutPage(){
+        logoutPage.openLogoutPage();
+    }
+
+    //-------testing if login page directs to the Send Assessment page----------
+    @Test
+    public void successfullLogin(){
+        Assertions.assertEquals("https://eng61.spartaglobal.academy/",loginPage.openLoginPage().login().openSendAssessPage().getURL());
+    }
+//===========TESTING ENTERING FIELDS============
+
+    //-----Testing right credentials input
     @Test
     public void enterRightCredentials(){
-        loginPage.openLoginPage();
         loginPage.submitRightCredentials();
+        Assertions.assertEquals(false, loginPage.errorMessage());
     }
     @Test
     public void enterWrongUsername(){
-        loginPage.openLoginPage();
         loginPage.submitWrongUsername();
         Assertions.assertEquals(true, loginPage.errorMessage());
     }
     @Test
     public void enterWrongPassword(){
-        loginPage.openLoginPage();
         loginPage.submitWrongPassword();
         Assertions.assertEquals(true, loginPage.errorMessage());
     }
     @Test
     public void enterNoInfo(){
-        loginPage.openLoginPage();
         loginPage.submitNoInfo();
         Assertions.assertEquals(true, loginPage.errorMessage());
     }
+    @Test
+    public void onlyUserName(){
+        loginPage.submitonlyUsername();
+        Assertions.assertEquals(true, loginPage.errorMessage());
+    }
+    @Test
+    public void onlyPassword(){
+        loginPage.submitonlyPassword();
+        Assertions.assertEquals(true, loginPage.errorMessage());
+    }
 
-
-//
-//    @Test
-//    @DisplayName("check Login Page Opens")
-//    public void inputUsername(){
-//        loginPage.openLoginPage();
-////        String loginURL = webDriver.getCurrentUrl();
-//        Assertions.assertEquals("https://eng61.spartaglobal.academy/",loginPage.login());
-//
-////
-//  }
 }
