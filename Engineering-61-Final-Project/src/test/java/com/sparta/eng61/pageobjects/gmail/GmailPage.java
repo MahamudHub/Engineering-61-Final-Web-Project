e8a2eb2f00b4a9cf6d8b154e146061c1821998ed
Raja Roy chowdhury <raja@rajas-mbp.home>
2020-08-06 14:01:46 +0100

    Updated method for Accessing Assessment Link from email. All working code.

--- Engineering-61-Final-Project/src/test/java/com/sparta/eng61/pageobjects/gmail/GmailPage.java
+++ Engineering-61-Final-Project/src/test/java/com/sparta/eng61/pageobjects/gmail/GmailPage.java
@@ -2,8 +2,7 @@ package com.sparta.eng61.pageobjects.gmail;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 import java.util.concurrent.TimeUnit;
 
