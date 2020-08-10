f0dde5ff13b21e56d2855a68bda6f23c5581485e
MahamudHub <mo.mahamud@outlook.com>
2020-08-10 11:59:33 +0100

    RoundTripTest class commit

---
+++ Engineering-61-Final-Project/src/test/java/com/sparta/eng61/RoundTripTest.java
@@ -0,0 +1,30 @@
+package com.sparta.eng61;
+
+import com.sparta.eng61.pageobjects.codingame.CodingamePage;
+import com.sparta.eng61.pageobjects.dispatcher.*;
+import com.sparta.eng61.pageobjects.gmail.GmailPage;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class RoundTripTest {
+    WebDriver webDriver = new ChromeDriver();
+    LoginPage loginPage = new LoginPage(webDriver);
+    DispatchesPage dispatchesPage = new DispatchesPage(webDriver);
+    ResultsPage resultsPage = new ResultsPage(webDriver);
+    PollsPage pollsPage = new PollsPage(webDriver);
+    SendAssessmentPage sendAssessmentPage = new SendAssessmentPage(webDriver);
+    LogoutPage logoutPage = new LogoutPage(webDriver);
+    GmailPage gmailPage = new GmailPage(webDriver);
+    CodingamePage codingamePage = new CodingamePage(webDriver);
+
+    @Test
+    public void roundTrip (){
+        loginPage.login()
+                .enterFields();
+        dispatchesPage.openDispatchesPage();
+        gmailPage.accessAssessmentLinkFromEmail().doTest();
+        resultsPage.clickUpdate();
+        resultsPage.openResultsPage();
+    }
+}
