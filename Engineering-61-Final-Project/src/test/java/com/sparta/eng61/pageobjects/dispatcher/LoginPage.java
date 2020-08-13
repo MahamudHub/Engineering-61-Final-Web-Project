9391ecd4915773b6ef8aab02daa60dca9a4eac4f
S-Mahamed <sumaya_abdalle@outlook.com>
2020-08-13 15:03:11 +0100

    commit 13/08

--- Engineering-61-Final-Project/src/test/java/com/sparta/eng61/pageobjects/dispatcher/LoginPage.java
+++ Engineering-61-Final-Project/src/test/java/com/sparta/eng61/pageobjects/dispatcher/LoginPage.java
@@ -114,9 +114,10 @@ public class LoginPage {
         return new SendAssessmentPage(webDriver);
     }
     public LoginPage reopenLoginPage () {
+
         webDriver.manage().window();
         webDriver.get("https://eng61.spartaglobal.academy/login");
-        //webDriver.switchTo().alert().accept();
+        webDriver.switchTo().alert().accept();
         return this;
     }
     public SendAssessmentPage relogin(){
